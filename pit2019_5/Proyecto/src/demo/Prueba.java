64cef2a2074a63a3e25dfa4fa5348b7534ba6af7
PieAlo <65374592+PieAlo@users.noreply.github.com>
2020-05-14 19:35:34 -0500

    Add files via upload

---
+++ pit2019_5/Proyecto/src/demo/Prueba.java
@@ -0,0 +1,15 @@
+﻿package demo;
+
+public class Prueba {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo");  
+		System.out.println("Jorge Jacinto "); 
+		System.out.println("Jhonny Susaya ");   
+		System.out.println("Sede Norte ");  
+		System.out.println("Piero Diaz"); 
+
+	}
+}
